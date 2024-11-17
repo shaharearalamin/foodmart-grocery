c21721fcb8f059408564cc6c4e436cc474263e2f
shaharearalamin <shaharearalamin@gmail.com>
2024-11-17 18:15:58 +0600

    fixing slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,96 @@ $(".heroBannerSliderWrapper").owlCarousel({
     },
   },
 });
+// owl carousel slider for hero seciton 
+
+// catergory owl carousel slider 
+$(document).ready(function(){
+  // Initialize Owl Carousels for multiple sliders
+  $(".categorySliderWrapper").each(function() {
+    var owl = $(this); // Each individual carousel
+
+    owl.owlCarousel({
+      items: 1, // Default to 1 item
+      loop: true,
+      margin: 10,
+      nav: false, // Disable default nav
+      dots: false, // Disable dots
+      responsive: {
+        0: {
+          // For screens 0px to 600px
+          items: 1,
+        },
+        600: {
+          // For screens 600px to 1000px
+          items: 3,
+        },
+        900: {
+          // For screens 1000px and above
+          items: 4,
+        },
+        1200: {
+          // For screens 1000px and above
+          items: 6,
+        },
+      },
+    });
+  });
+
+  // Custom next and prev button functionality
+  $(".owl-next, .owl-prev").click(function() {
+    var target = $(this).data('target'); // Get the target slider (e.g., category1, category2)
+    var owl = $("." + target); // Select the corresponding owl-carousel based on the target
+
+    if ($(this).hasClass('owl-next')) {
+      owl.trigger('next.owl.carousel');
+    } else if ($(this).hasClass('owl-prev')) {
+      owl.trigger('prev.owl.carousel');
+    }
+  });
+});
+
+// catergory owl carousel slider 
+
+// Brand owl carousel slider 
+$(document).ready(function(){
+  // Initialize Owl Carousels for multiple sliders
+  $(".BrandSliderWrapper").each(function () {
+    var owl = $(this); // Each individual carousel
+
+    owl.owlCarousel({
+      items: 1, // Default to 1 item
+      loop: true,
+      margin: 10,
+      nav: false, // Disable default nav
+      dots: false, // Disable dots
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        900: {
+          items: 3,
+        },
+        1200: {
+          items: 4,
+        },
+      },
+    });
+  });
+
+  // Custom next and prev button functionality
+  $(".owl-next, .owl-prev").click(function() {
+    var target = $(this).data('target'); // Get the target slider (e.g., category1, category2)
+    var owl = $(this).closest(".container-fluid").find("." + target); // Find the specific owl-carousel in the same section
+
+    if ($(this).hasClass('owl-next')) {
+      owl.trigger('next.owl.carousel');
+    } else if ($(this).hasClass('owl-prev')) {
+      owl.trigger('prev.owl.carousel');
+    }
+  });
+});
+
+// Brand owl carousel slider 
